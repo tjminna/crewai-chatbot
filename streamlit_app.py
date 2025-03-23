b992f04fd94ade4ca9c57cd4db03d8439ba5ec2e
tjminna <tjminna6724@gmail.com>
2025-03-23 11:45:22 +0530

    added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
+# SQLite fix for ChromaDB
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 import streamlit as st
 from crewai import Agent, Task, Crew
 from langchain_community.chat_models import ChatOpenAI
